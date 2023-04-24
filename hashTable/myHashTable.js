b7c48feb5892e6478c5e0a52b29ef662ac48ff57
Si Thu Win <sthuwin@gmail.com>
2023-04-24 21:02:45 +1000

    create my hash table

--- hashTable/myHashTable.js
+++ hashTable/myHashTable.js
@@ -11,8 +11,34 @@ class MyHashTable {
     return hash;
   }
 
+  set(key, value) {
+    let address = this._hash(key);
+    if (!this.data[address]) {
+      this.data[address] = [];
+      this.data[address].push([key, value]);
+    } 
+    this.data[address].push([key, value]);
+    console.log(this.data);
+    return this.data;
+  } // O(1) if no collisions
+
+  get(key) {
+    let address = this._hash(key);
+    const currentBucket = this.data[address];
+    if (currentBucket) {
+      for (let i=0; i<currentBucket.length; i++) {
+        if (currentBucket[i][0] === key) {
+          return currentBucket[i][1]
+        }
+      }
+    } // 0(1) if no collisions
+
+    return undefined
+  }
+
 }
 
 const myHashTable = new MyHashTable(50);
 myHashTable.set("grapes", 10000);
-myHashTable.get("grapes");
+myHashTable.set("apples", 10000);
+console.log(myHashTable.get("grapes"));
