bf21630247e422806219bf8f9e2dee8f243504d2
Si Thu Win <sthuwin@gmail.com>
2023-04-26 20:49:15 +1000

    find first recurring character with 2 loops

---
+++ hashTable/firstRecurringNumber.js
@@ -0,0 +1,12 @@
+function firstRecurringCharacter(input) {
+  for (let i = 0; i < input.length; i++) {
+    for (let j = i + 1; j < input.length; j++) {
+      if (input[i] === input[j]) {
+        return input[i];
+      }
+    }
+  }
+  return undefined;
+} // O(n^2)
+
+console.log(firstRecurringCharacter([2, 5, 1, 2, 3, 5, 1, 2, 4]));
