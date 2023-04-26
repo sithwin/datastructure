eac94ad959ea5f1df6c03a84c481230f16e96fe6
Si Thu Win <sthuwin@gmail.com>
2023-04-26 20:28:58 +1000

    get all the values from hashTable

--- hashTable/myHashTable.js
+++ hashTable/myHashTable.js
@@ -36,9 +36,43 @@ class MyHashTable {
     return undefined
   }
 
+  keys() {
+    const keysArray = [];
+    for (let i=0; i<this.data.length; i++) {
+      if (this.data[i]) {
+        keysArray.push(this.data[i][0][0])
+      }
+    }
+    return keysArray;
+  } // O(n)
+
+
+  keysWithoutCollison() {
+    if (!this.data.length) {
+      return undefined
+    }
+    let result = []
+    // loop through all the elements
+    for (let i = 0; i < this.data.length; i++) {
+        // if it's not an empty memory cell
+        if (this.data[i] && this.data[i].length) {
+          // but also loop through all the potential collisions
+          if (this.data.length > 1) {
+            for (let j = 0; j < this.data[i].length; j++) {
+              result.push(this.data[i][j][0])
+            }
+          } else {
+            result.push(this.data[i][0])
+          } 
+        }
+    }
+    return result; 
+  }
+
 }
 
 const myHashTable = new MyHashTable(50);
 myHashTable.set("grapes", 10000);
 myHashTable.set("apples", 10000);
+myHashTable.set("oranges", 10000);
 console.log(myHashTable.get("grapes"));
